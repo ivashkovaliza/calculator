eb1e1ecd65287d53526c591c8ce4760ce390f545
ivashkovaliza <31897547+ivashkovaliza@users.noreply.github.com>
2018-08-10 17:50:51 +0300

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,49 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    context: path.resolve(__dirname, 'src'),
-    entry: ["./index.js", "./index.html"],
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: "bundle.js",
-    },
-    watch: true,
-    module: {
-        rules: [{
-            test: /\.scss$/,
-            loader: 'style-loader!css-loader!sass-loader',
-        },  {
-            test: /\.(png|jpg|gif)$/,
-            use: [
-                {
-                    loader: 'file-loader',
-                    options: {
-                        limit: 8192
-                    }
-                }
-            ]
-        }, {
-            test: /\.(html)$/,
-            use: {
-                loader: 'html-loader',
-                options: {
-
-                }
-            }
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+  entry: ["./index.js", "./index.html"],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "bundle.js",
+  },
+  watch: true,
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader
+        },
+        'css-loader',
+        'sass-loader',
+      ]
+    },  {
+      test: /\.(png|jpg|gif)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+              limit: 8192
+          }
         }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({template: "./index.html", inject: true,}),
-    ],
+      ]
+    }, {
+      test: /\.(html)$/,
+      use: {
+        loader: 'html-loader',
+        options: {
+
+        }
+      }
+    }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: "./index.html", inject: true,}),
+    new MiniCssExtractPlugin(),
+  ],
 };
